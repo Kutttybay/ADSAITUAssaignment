7d8e566e87d3c0711901b54347cd17d03701c15f
kutttybay <kutttybay@gmail.com>
2023-04-09 15:14:51 +0600

    8 commit

--- src/algosAss1/Task7.java
+++ src/algosAss1/Task7.java
@@ -3,6 +3,13 @@ package algosAss1;
 import java.util.Scanner;
 
 public class Task7 {
+    /*
+    The code checks if n (the number of elements in the array) is 0, then returns the first element of the array arr[0],
+    since it is the last element in reverse order.
+    Otherwise, the function prints the current array element arr[n-1] using Sout and calls itself with an argument (n-1) to
+    continue printing the array elements in reverse order.
+    The function continues to call itself recursively until all elements of the array have been printed in reverse order.
+    */
     public int reversedNumbers(int n, int[] arr){
         if (n == 0){
             return arr[0];
