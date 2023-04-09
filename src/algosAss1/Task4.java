31327e147f7ea454e324d9b5af7097510cffcd9d
kutttybay <kutttybay@gmail.com>
2023-04-09 15:01:47 +0600

    5 commit

--- src/algosAss1/Task4.java
+++ src/algosAss1/Task4.java
@@ -3,6 +3,11 @@ package algosAss1;
 import java.util.Scanner;
 
 public class Task4 {
+    /*
+    The code checks if the number n is 0, then returns 1, since the factorial of 0 is 1.
+    Otherwise, the function uses recursion to call itself with the argument (n - 1), multiplying the current value by n.
+    The function continues to call itself recursively until n is 0 and returns the product of all numbers from 1 to the original value of n.
+    */
     public static int factorial(int n) {
         if (n == 0) {
             return 1;
